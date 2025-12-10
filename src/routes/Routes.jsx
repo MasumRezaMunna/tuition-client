9bbe0462e6acf863abff87e56a6ca1c79bb99044
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-12-10 10:26:57 +0600

    set up nested routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Auth/Login";
+import Register from "../pages/Auth/Register";
+import Dashboard from "../layout/Dashboard";
+import ErrorPage from "../pages/Shared/ErrorPage";
 
-const Routes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
 
-export default Routes;
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>, 
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'register',
+        element: <Register></Register>
+      }
+    ]
+  },
+  {
+    path: "dashboard",
+    element: <Dashboard></Dashboard>, 
+    children: [
+      {
+        path: 'my-tuitions',
+        element: <div>My Tuitions Page</div> 
+      },
+      {
+        path: 'users',
+        element: <div>Manage Users Page</div>
+      },
+      {
+        path: 'my-applications',
+        element: <div>My Applications Page</div>
+      }
+    ]
+  }
+]);
