a8fddfbddeb8c4e1c6607d94177292ada162281a
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-12-10 10:35:40 +0600

    added a dummy state

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
-
-const AuthProvider = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import { createContext } from "react";
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+    const authInfo = { user: null, loading: true };
+    return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
 };
-
 export default AuthProvider;
