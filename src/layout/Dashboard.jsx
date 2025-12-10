82a6e767b608667c7511b579ba34ea0cc2fba3c5
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-12-10 11:10:46 +0600

    basic page set up

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-
+import { Outlet } from "react-router-dom";
 const Dashboard = () => {
     return (
-        <div>
-            
+        <div className="flex">
+            <div className="w-64 bg-base-200 min-h-screen">Sidebar up</div>
+            <div className="flex-1 p-8">
+                <Outlet />
+            </div>
         </div>
     );
 };
-
 export default Dashboard;
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-
+import { Outlet } from "react-router-dom";
 const Main = () => {
     return (
         <div>
-            
+            <h2>Navbar Placeholder</h2>
+            <Outlet />
+            <h2>Footer Placeholder</h2>
         </div>
     );
 };
-
 export default Main;
