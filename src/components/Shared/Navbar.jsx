fbf4d6cfc541e218ef2a6086c277dc0a2d835166
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-12-20 13:47:38 +0600

    added myprofile link to navbar

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -3,71 +3,115 @@ import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
 
 const Navbar = () => {
-    const { user, logOut } = useContext(AuthContext);
+  const { user, logOut } = useContext(AuthContext);
 
-    const handleLogOut = () => {
-        logOut()
-            .then(() => { })
-            .catch(error => console.log(error));
-    }
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {})
+      .catch((error) => console.log(error));
+  };
 
-    const navOptions = <>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/tuitions">Tuitions</NavLink></li>
-        <li><NavLink to="/tutors">Tutors</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
+  const navOptions = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/tuitions">Tuitions</NavLink>
+      </li>
+      <li>
+        <NavLink to="/tutors">Tutors</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About</NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact">Contact</NavLink>
+      </li>
     </>
+  );
 
-    return (
-        <div className="navbar fixed w-full z-10 bg-opacity-30 bg-black text-white">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-white">
-                        {navOptions}
-                    </ul>
-                </div>
-                <Link to="/" className="btn btn-ghost normal-case text-xl">
-                    <img src="https://i.ibb.co/m5MYT3Pn/tuition-logo.png" alt="logo" className="w-8 h-8 rounded-full" />
-                    eTuitionBd
-                </Link>
-            </div>
-            
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {navOptions}
-                </ul>
-            </div>
-
-            <div className="navbar-end">
-                {user ? (
-                    <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img src={user?.photoURL || "https://i.ibb.co/hYsm3Dq/default-user.png"} alt="profile" />
-                            </div>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-white">
-                            <li>
-                                <Link to="/dashboard" className="justify-between">
-                                    Dashboard
-                                    <span className="badge">New</span>
-                                </Link>
-                            </li>
-                            <li><button onClick={handleLogOut}>Logout</button></li>
-                        </ul>
-                    </div>
-                ) : (
-                    <Link to="/login">
-                        <button className="btn btn-active btn-neutral text-white">Login</button>
-                    </Link>
-                )}
-            </div>
+  return (
+    <div className="navbar fixed w-full z-10 bg-opacity-30 bg-black text-white">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-1 p-2 shadow bg-base-100 rounded-box w-52 text-white"
+          >
+            {navOptions}
+          </ul>
         </div>
-    );
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+          <img
+            src="https://i.ibb.co/m5MYT3Pn/tuition-logo.png"
+            alt="logo"
+            className="w-8 h-8 rounded-full"
+          />
+          eTuitionBd
+        </Link>
+      </div>
+
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navOptions}</ul>
+      </div>
+
+      <div className="navbar-end">
+        {user ? (
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  src={
+                    user?.photoURL ||
+                    "https://i.ibb.co/hYsm3Dq/default-user.png"
+                  }
+                  alt="profile"
+                />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-1 p-2 shadow bg-base-100 rounded-box w-52 text-white"
+            >
+              <li>
+                <Link to="/dashboard" className="justify-between">
+                  Dashboard
+                  <span className="badge">New</span>
+                </Link>
+              </li>
+              <li><Link to="/dashboard/profile">My Profile</Link></li>
+              <li>
+                <button onClick={handleLogOut}>Logout</button>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <Link to="/login">
+            <button className="btn btn-active btn-neutral text-white">
+              Login
+            </button>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
